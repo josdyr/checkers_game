c021d25107bfac2c98c6c4f182c9c33712e834e4
Jostein Dyrseth <josdyr@josdyr-mac.local>
2017-11-02 11:41:10 +0000

    A few minor changes

--- checkers_game.py
+++ checkers_game.py
@@ -1,21 +1,30 @@
-white_initial_positions = [
-    "A1", "A3", "B2", "C1", "C3", "D2", "E1", "E3", "F2", "G1", "G3", "H2"
-]
-black_initial_positions = [
-    "A7", "B6", "B8", "C7", "D6", "D8", "E7", "F6", "F8", "G7", "H6", "H8"
-]
+import string
+
+white_initial_positions = ["A1", "A3", "B2", "C1", "C3", "D2", "E1", "E3", "F2", "G1", "G3", "H2"]
+black_initial_positions = ["A7", "B6", "B8", "C7", "D6", "D8", "E7", "F6", "F8", "G7", "H6", "H8"]
+board = [[None] * 8 for i in range(8, 0, -1)]
 
 
-board = [[None] * 8] * 8
-letters = ["A", "B", "C", "D", "E", "F", "G", "H"]
-numbers = ["1", "2", "3", "4", "5", "6", "7", "8"]
-
+###########
+# CLASSES #
+###########
 
 class Slot(object):
+
+    # def __init__(self, piece, location):
+    #     self.piece = piece
+    #     self.location = location
     pass
 
 
-# Creates a coordinate. Methods: move
+class Piece(object):
+
+    def __init__(self, color, location, player):
+        self.color = color
+        self.location = location
+        self.player = player
+
+
 class Point(object):
 
     def __init__(self, row, col):
@@ -30,24 +39,8 @@ class Point(object):
         return "Point: {}, {}".format(self.row, self.col)
 
 
-class Piece(object):
-    position = ""
-    location = Point()
-
-    def __init__(self, color, position):
-        self.color = color
-        self.position = position
-
-
-class Move(object):
-    def __init__(self, row, col, player):
-        self.row = row
-        self.col = col
-        self.player = player
-
-
 class Player(object):
-    color = 'white'
+    color = None
     pieces = 12
 
     def __init__(self, color, pieces):
@@ -55,11 +48,25 @@ class Player(object):
         self.pieces = pieces
 
 
+#############
+# FUNCTIONS #
+#############
+
+def make_board(board):
+    for row in board:
+        for col in row:
+            row[col] = 1
+
+    return board
+
+
 def draw_board():
     i = 8
     for row in board:
-        print("{} ".format(i), end='')
+        print("{} ".format(i), end='')  # Print the margin numbers
         i = i - 1
+
+        # Print '[ ]' OR print the content:
         for col in row:
             if col is None:
                 print('[ ]', end='')
@@ -67,7 +74,9 @@ def draw_board():
                 print(col, end='')
         print('')
     print(' ', end="")
-    alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+
+    # Print the characters
+    alphabet = string.ascii_uppercase[:8]
     for l in alphabet:
         print("  {}".format(l), end="")
     print()
