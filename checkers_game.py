bb51aa5f840e83229e80d0ef81468e2cc9703a54
Jostein Dyrseth <josdyr@josdyr-mac.local>
2017-11-07 20:15:53 +0000

    A big change

--- checkers_game.py
+++ checkers_game.py
@@ -1,4 +1,3 @@
-import numpy
 import os
 import pdb
 import string
@@ -11,16 +10,25 @@ import string
 
 class Space(object):
     piece = None
-    location = ""
     coordinates = (None, None)
 
 
 class Piece(object):
     player = ""
-    king = False
+    _type = None
 
-    def __init__(self, player):
-        self.player = player
+    def __init__(self, _type):
+        self.player = _type
+
+    def get_display_value(self):
+        if self._type is None:
+            return self.player[0]
+        else:
+            return self._type[0]
+
+
+class Player(object):
+    pass
 
 
 #############
@@ -40,56 +48,35 @@ def make_board():
 
 
 def give_location(board):
-    # make range 1 - 8
-    # numbers = list(range(8))
-    numbers = list(range(8, 0, -1))
-    # make range A - H
-    letters = string.ascii_uppercase[:8]
 
     for row_idx, row in enumerate(board):
         for space_idx, space in enumerate(row):
-
-            # give locations to each space
-            current_location = ""
-            current_location += str(letters[space_idx])
-            current_location += str(numbers[row_idx])
-            space.location = current_location
 
             # give coordinates to each space
             space.coordinates = (row_idx, space_idx)
 
 
 def place_pieces(board):
-    white_initial_positions = ["A1", "A3", "B2", "C1",
-                               "C3", "D2", "E1", "E3", "F2", "G1", "G3", "H2"]
-    black_initial_positions = ["A7", "B6", "B8", "C7",
-                               "D6", "D8", "E7", "F6", "F8", "G7", "H6", "H8"]
 
-    # white_initial_positions = [
-    #     (0, 0), (2, 0), (1, 1), (0, 2), (2, 2), (1, 3), (0, 4), (2, 4), (1, 5), (0, 6), (2, 6), (1, 7)
-    # ]
-    # black_initial_positions = [
-    #     (6, 0), (5, 1), (7, 1), (6, 2), (5, 3), (7, 3), (6, 4), (5, 5), (7, 5), (6, 6), (5, 7), (7, 7)
-    # ]
-    #
-    # for location in white_initial_positions:
-    #     board[location[0]][location[1]].piece = Piece("White")
-    #
-    # for location in black_initial_positions:
-    #     board[location[0]][location[1]].piece = Piece("Black")
+    white_initial_positions = [
+        (0, 0), (2, 0), (1, 1), (0, 2), (2, 2), (1, 3), (0, 4), (2, 4), (1, 5), (0, 6), (2, 6), (1, 7)
+    ]
+    black_initial_positions = [
+        (6, 0), (5, 1), (7, 1), (6, 2), (5, 3), (7, 3), (6, 4), (5, 5), (7, 5), (6, 6), (5, 7), (7, 7)
+    ]
 
     # if location in lists is the same as the space in board:
     for row_idx, row in enumerate(board):
         for space_idx, space in enumerate(row):
-            if board[row_idx][space_idx].location in white_initial_positions:
+            if board[row_idx][space_idx].coordinates in white_initial_positions:
                 board[row_idx][space_idx].piece = Piece('White')
-            elif board[row_idx][space_idx].location in black_initial_positions:
+            elif board[row_idx][space_idx].coordinates in black_initial_positions:
                 board[row_idx][space_idx].piece = Piece('Black')
     return board
 
 
 def draw_board():
-    os.system('clear')
+    # os.system('clear')
     for row_idx, row in enumerate(board):
         print("{} ".format(row_idx), end='')  # Print the index number (debug)
         print("{} ".format(row_idx + 1), end='')  # Print the margin game-numbers
@@ -97,7 +84,7 @@ def draw_board():
         # Print '[ ]' OR print the content:
         for space_idx, space in enumerate(row):
             if space.piece:  # if Piece in space
-                print('[{}]'.format(board[row_idx][space_idx].piece.player[0]), end="")
+                print('[{}]'.format(board[row_idx][space_idx].piece.get_display_value()), end="")
             else:
                 print("[ ]", end="")
         print("")
@@ -116,7 +103,15 @@ def draw_board():
     print()
 
 
-def move(piece, to):
+def get_board_piece(coor):
+    return board[coor[0]][coor[1]].piece
+
+
+def set_board_piece(coor, value):
+    board[coor[0]][coor[1]].piece = value
+
+
+def map_coordinates(arg):
 
     abc_map = {
         'A': 0,
@@ -129,65 +124,82 @@ def move(piece, to):
         'H': 7
     }
 
-    from_x = int(piece[1]) - 1  # from_x
-    from_y = abc_map[piece[0]]  # from_y
+    x = int(arg[1]) - 1
+    y = abc_map[arg[0]]
 
-    to_x = int(to[1]) - 1
-    to_y = abc_map[to[0]]
+    return (x, y)
 
-    # check if space has a piece
-    if board[from_x][from_y].piece is not None:
 
-        player = board[from_x][from_y].piece.player
-        king = board[from_x][from_y].piece.king
-        coor_from = board[from_x][from_y].coordinates
-        coor_to = board[to_x][to_y].coordinates
+def move(from_space, to_space):
 
-        if king:
-            displacements = [(-1, -1), (-1, 1), (1, 1), (1, -1), (-2, -2), (-2, 2), (2, 2), (2, -2)]
+    # pdb.set_trace()
 
-        if player == "White":
-            displacements = [(-1, -1), (-1, 1), (-2, -2), (-2, 2)]
+    coor_from = map_coordinates(from_space)
+    print(coor_from)
+    coor_to = map_coordinates(to_space)
+    piece = get_board_piece(coor_from)
 
-            # # for each displacement, add it to the from locaiton
-            # # Generates a list of all possible moves
-            # possibleLocations = [(piece[0] + x[0], piece[1] + x[1]) for x in displacements]
-            # possibleLocations = [x for x in possibleLocations if x[0]
-            #                      >= 0 and x[0] < 8 and x[1] >= 0 and x[1] < 8]
-            # if to in possibleLocations:
-            #     # Move is valid!
-            #     do_move(from_x, from_y, to_x, to_y, player)
-            # # if coor_from == tuple(numpy.subtract(coor_to, (-1, 1))) or coor_from == tuple(numpy.subtract(coor_to, (-1, -1))):
-            # #    do_move(from_x, from_y, to_x, to_y, player)
+    # check if space has a from_space
+    if piece is not None:
+        player = piece.player
+        current_type = piece._type
 
-            if coor_from == tuple(numpy.subtract(coor_to, (-1, 1))) or coor_from == tuple(numpy.subtract(coor_to, (-1, -1))):
-                do_move(from_x, from_y, to_x, to_y, player)
+        # make king a type variable
+        # make a get_dispacements(type, player)
+        displacements = get_dispacements(current_type, player)
 
-        elif player == "Black":
-            displacements = [(1, 1), (1, -1), (2, 2), (2, -2)]
+        # get_valid_moves(coor_from, displacements)
+        valid_moves = get_valid_moves(coor_from, displacements)
 
-            if coor_from == tuple(numpy.subtract(coor_to, (1, 1))) or coor_from == tuple(numpy.subtract(coor_to, (1, -1))):
-                do_move(from_x, from_y, to_x, to_y, player)
+        if coor_to in valid_moves:
+            do_move(coor_from, coor_to, piece)
+
+            if check_king(coor_to) is True:
+                board[coor_to[0]][coor_to[1]].piece._type = "King"
+        else:
+            print('Invalid move.')
+
     else:
-        print("Error: Current space has no piece")
-
-    draw_board()
-    print("from: ({}, {})".format(from_x, from_y))
-    print("to: ({}, {})".format(to_x, to_y))
+        print("Local Error: Current space has no piece")
 
 
-def do_move(from_x, from_y, to_x, to_y, player):
-    board[from_x][from_y].piece = None  # set piece to None
-    board[to_x][to_y].piece = Piece(player)  # make a new Piece
+def do_move(coor_from, coor_to, piece):
+    set_board_piece(coor_to, piece)
+    set_board_piece(coor_from, None)
 
 
-def possible_moves(piece):
-    """returns a list of possible moves"""
-    pass
+def get_dispacements(current_type, player):
+    if current_type == "king":
+        displacements = [(-1, -1), (-1, 1), (1, 1), (1, -1), (-2, -2), (-2, 2), (2, 2), (2, -2)]
+    elif player == "White":
+        displacements = [(1, -1), (1, 1), (2, -2), (2, 2)]
+    elif player == "Black":
+        displacements = [(-1, -1), (-1, 1), (-2, -2), (-2, 2)]
+    return displacements
 
 
-def _move(space, to):
-    pass
+def get_valid_moves(coor_from, displacements):
+    valid_moves = [(coor_from[0] + coor[0], coor_from[1] + coor[1])
+                   for coor in displacements]
+    valid_moves = [coor for coor in valid_moves if coor[0]
+                   >= 0 and coor[0] < 8 and coor[1] >= 0 and coor[1] < 8]
+    return valid_moves
+
+
+def game_loop():
+    while True:
+        draw_board()
+        from_space = input('What piece do you want to move from: ')
+        to_space = input('What piece do you want to move to: ')
+        move(from_space, to_space)
+
+
+def check_king(coor_to):
+    pdb.set_trace()
+    if coor_to[0] == 0 or coor_to[0] == 7:
+        return True
+    else:
+        return False
 
     ###########
     # PROGRAM #
@@ -195,13 +207,13 @@ def _move(space, to):
 
 
 board = make_board()
+
+move('B6', 'C5')
+move('C5', 'B4')
+move('B4', 'C3')
+move('C3', 'B2')
+move('B2', 'C1')
+
 draw_board()
 
-pdb.set_trace()
-
-# testing moves:
-move('C6', 'B5')
-
-move('D3', 'E4')
-
-move('E6', 'F5')
+# game_loop()
